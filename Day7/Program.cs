ef24c860b2990b6bee0ad18bb16791636491141d
Calle20 <97348165+Calle20@users.noreply.github.com>
2023-12-08 21:27:32 +0100

    Day 7 p2 finally working

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -9,7 +9,6 @@ int sum1 = 0;
 int sum2 = 0;
 
 List<Hand> hands = new List<Hand>();
-List<int> bids=new List<int>();
 
 foreach (string line in lines)
 {
@@ -19,21 +18,52 @@ foreach (string line in lines)
         cards.Add(new Card(element.ToString()));
     });
     int bid=int.Parse(line.Split(" ", StringSplitOptions.RemoveEmptyEntries)[1]);
-    hands.Add(new Hand(cards, bid));
+    hands.Add(new Hand(cards, bid, false));
 }
 
+//Part1
 hands = hands.OrderBy(e => e.HandType)
-                   .ThenBy(e => e.Cards[0].Value)
-                   .ThenBy(e => e.Cards[1].Value)
-                   .ThenBy(e => e.Cards[2].Value)
-                   .ThenBy(e => e.Cards[3].Value)
-                   .ThenBy(e => e.Cards[4].Value)
+                   .ThenBy(e => e.Cards[0].getCardValue(false))
+                   .ThenBy(e => e.Cards[1].getCardValue(false))
+                   .ThenBy(e => e.Cards[2].getCardValue(false))
+                   .ThenBy(e => e.Cards[3].getCardValue(false))
+                   .ThenBy(e => e.Cards[4].getCardValue(false))
                    .ToList();
 
 hands.Select((e, index) => { e.Rank = index + 1; return e; }).ToList();
 
 sum1 = hands.Sum(e => e.Winning);
 Console.WriteLine(sum1);
+
+//Part2
+
+hands.Clear();
+
+foreach (string line in lines)
+{
+    List<Card> cards = new List<Card>();
+    line.Split(" ", StringSplitOptions.RemoveEmptyEntries)[0].ToCharArray().ToList().ForEach((element) =>
+    {
+        cards.Add(new Card(element.ToString()));
+    });
+    int bid = int.Parse(line.Split(" ", StringSplitOptions.RemoveEmptyEntries)[1]);
+    hands.Add(new Hand(cards, bid, true));
+}
+
+
+hands.Select(hand => { hand.getHandType(hand.Cards, true); return hand; });
+hands = hands.OrderBy(e => e.HandType)
+                   .ThenBy(e => e.Cards[0].getCardValue(true))
+                   .ThenBy(e => e.Cards[1].getCardValue(true))
+                   .ThenBy(e => e.Cards[2].getCardValue(true))
+                   .ThenBy(e => e.Cards[3].getCardValue(true))
+                   .ThenBy(e => e.Cards[4].getCardValue(true))
+                   .ToList();
+
+hands.Select((e, index) => { e.Rank = index + 1; return e; }).ToList();
+
+sum2 = hands.Sum(e => e.Winning);
+
 Console.WriteLine(sum2);
 
 enum HandType
@@ -44,30 +74,31 @@ enum HandType
 class Card
 {
     public string Name;
-    public int Value { 
-        get {
-            if(int.TryParse(Name, out int res))
-            {
-                return res-2;
-            }
-            else
-            {
-                switch (Name)
-                {
-                    case "A": return 12;
-                    case "K": return 11;
-                    case "Q": return 10;
-                    case "J": return 9;
-                    case "T": return 8;
-                }
-            }
-            return 0;
-        } 
-    }
+    public int Value;
 
     public Card(string name)
     {
         Name = name;
+    }
+
+    public int getCardValue(bool jIsJoker)
+    {
+        if (int.TryParse(Name, out int res))
+        {
+            return res;
+        }
+        else
+        {
+            switch (Name)
+            {
+                case "A": return 14;
+                case "K": return 13;
+                case "Q": return 12;
+                case "J": return jIsJoker ? 1 : 11;
+                case "T": return 10;
+            }
+        }
+        return 0;
     }
 }
 
@@ -77,17 +108,17 @@ class Hand
     public HandType HandType;
     public List<Card> Cards;
     public int Rank=0;
-    public int Bit=0;
-    public int Winning { get { return Bit * Rank; }}
+    public int Bid=0;
+    public int Winning { get { return Bid * Rank; }}
 
-    public Hand(List<Card> cards, int bit)
+    public Hand(List<Card> cards, int bid, bool isPart2)
     {
         Cards = cards;
-        HandType = getHandType(cards);
-        Bit = bit;
+        getHandType(cards, isPart2);
+        Bid = bid;
     }
 
-    private HandType getHandType(List<Card> cards) 
+    public void getHandType(List<Card> cards, bool isPart2) 
     {
         Dictionary<char, int> labelCount = new Dictionary<char, int>();
 
@@ -103,15 +134,47 @@ class Hand
         switch (distinctLabels)
         {
             case 1:
-                return HandType.FiveOfAKind;
+                HandType = HandType.FiveOfAKind;
+                break;
             case 2:
-                return labelCount.ContainsValue(4) ? HandType.FourOfAKind : HandType.FullHouse;
+                HandType = labelCount.ContainsValue(4) ? HandType.FourOfAKind : HandType.FullHouse;
+                break;
             case 3:
-                return labelCount.ContainsValue(3) ? HandType.ThreeOfAKind : HandType.TwoPair;
+                HandType = labelCount.ContainsValue(3) ? HandType.ThreeOfAKind : HandType.TwoPair;
+                break;
             case 4:
-                return HandType.OnePair;
-            default:
-                return HandType.Highcard;
+                HandType = HandType.OnePair;
+                break;
+            case 5:
+                HandType = HandType.Highcard; 
+                break;
+        }
+        if (!isPart2)
+            return;
+        if(cards.Any(e=> e.Name == "J"))
+        {
+            if (HandType == HandType.FourOfAKind)
+                HandType = HandType.FiveOfAKind;
+            else if (HandType == HandType.FullHouse)
+            {
+                if (cards.Count(e => e.Name == "J") == 1)
+                    HandType = HandType.FourOfAKind;
+                else
+                    HandType = HandType.FiveOfAKind;
+            }
+            else if (HandType == HandType.ThreeOfAKind)
+                HandType = HandType.FourOfAKind;
+            else if (HandType == HandType.TwoPair)
+            {
+                if (cards.Count(e => e.Name == "J") == 1)
+                    HandType = HandType.FullHouse;
+                else
+                    HandType = HandType.FourOfAKind;
+            }
+            else if (HandType == HandType.OnePair) 
+                HandType = HandType.ThreeOfAKind;
+            else if (HandType == HandType.Highcard)
+                HandType = HandType.OnePair;
         }
     }
 }
